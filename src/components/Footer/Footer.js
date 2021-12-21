15ac95729edb7468f6fca70d3bbca98e1e988443
DanMartinez01 <martinezdl_@hotmail.com>
2021-12-21 16:06:24 -0300

    Add links to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 export const Footer = () => {
     return (
-
-        <footer className="footer">©OtroCielo Store 2021</footer>
+        <footer className="footer">
+            <div>
+                <div className='footerLinks'>
+                    <li className='footerLinks'><Link to='/'><b>Home</b></Link></li>
+                    <li className='footerLinks'><Link to='/category/Sunglasses'><b>Sunglasses</b></Link></li>
+                    <li className='footerLinks'><Link to='/category/Earrings'> <b>Earrings</b></Link></li>
+                    <li className='footerLinks'><Link to='/category/Sale'> <b>Sale!</b></Link></li>
+                </div>
+                <hr />
+                <p className='footerBrand' > ©OtroCielo Store 2021</p>
+            </div>
+        </footer>
     )
 }
